527dcd8a36ad2c21ae903f6be46ad4c18f070ff8
William-vale <william_vale@alu.ufc.br>
2024-08-07 15:00:53 -0300

    feat: add Header component and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-import { Button } from "./_components/ui/button"
+import Header from "./_components/header";
+import Image from "next/image";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import { SearchIcon } from "lucide-react";
 
 const Home = () => {
-  return <Button>Olá Mundo</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">
+          Olá, {/* Será Dinâmico */} William {/* Será Dinâmico */}
+        </h2>
+        <p>Quarta-feira, 08 de Agosto</p>
+
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca..." />
+          <Button size="icon">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 w-full h-[150px]">
+          <Image
+            alt="Agende nos melhores com o FSW Barber"
+            src="/Banner_pizza.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
